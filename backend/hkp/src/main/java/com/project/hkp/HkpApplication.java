cac115477f589dabcc00ceaf042dd7acc102469c
김민제 <loloy@Kovalt03.local>
2025-08-09 18:03:38 +0900

    start

---
+++ backend/hkp/src/main/java/com/project/hkp/HkpApplication.java
@@ -0,0 +1,13 @@
+package com.project.hkp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkpApplication.class, args);
+	}
+
+}
---
+++ backend/hkp/src/test/java/com/project/hkp/HkpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hkp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
